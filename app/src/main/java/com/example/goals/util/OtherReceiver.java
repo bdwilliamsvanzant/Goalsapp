2674d6591e49eb6b60e9b822f525ed8787e6a5a1
karllundgren <39705381+karllundgren@users.noreply.github.com>
2019-05-04 17:14:01 -0700

    set data backup to false. added alternative notification symbol

--- app/src/main/java/com/example/goals/util/OtherReceiver.java
+++ app/src/main/java/com/example/goals/util/OtherReceiver.java
@@ -8,12 +8,14 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
-
+import 	android.graphics.Color;
 import com.example.goals.R;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+
+import static android.graphics.Color.argb;
 
 public class OtherReceiver extends BroadcastReceiver {
 
@@ -23,12 +25,13 @@ public class OtherReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_home_black_24dp);
 
 
         Log.i("createNotification","entered");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "qwerty")
                 .setSmallIcon(R.drawable.ic_stat_name)
+                .setColor(argb(1,252,144,20))
                 .setLargeIcon(largeIcon)
                 .setContentTitle("Expiring goals")
                 .setContentText("you have a goal that expires today!!!!")
